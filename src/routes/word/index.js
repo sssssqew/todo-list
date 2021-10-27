b795a44bec7bd9d1839a04d8eace8739215737bf
sssssqew <sunrise.in.my.dream@gmail.com>
2021-10-27 19:49:26 +0900

    added plenty of query for mongo db

--- src/routes/word/index.js
+++ src/routes/word/index.js
@@ -10,7 +10,24 @@ WordRouter.route('/(:word)?').get( async (req, res) => {
     if(word !== "undefined" && word !== undefined){
         console.log(word)
         try{
-            words = await Word.find({ r_word: word}) // 데이터베이스에서 쿼리로 단어를 검색
+            // words = await Word.find({ r_word: word}) // Word 모델의 r_word 필드에서 쿼리와 일치하는 단어 검색
+            words = await Word.find({ r_word: { $regex: `^${word}`}}) // Word 모델의 r_word 필드에서 쿼리로 시작하는 단어 검색
+            // words = await Word.find({ r_word: { $regex: `${word}$`}}) // Word 모델의 r_word 필드에서 쿼리로 끝나는 단어 검색
+            // words = await Word.find({ r_des: { $regex: `${word}`}}) // Word 모델의 r_des 필드에서 쿼리를 포함하는 단어 검색
+            
+            // words = await Word.find({ $or: [      // Word 모델의 r_word 필드와 r_des 필드에서 쿼리를 포함하는 단어 검색
+            //     {r_word: {$regex: `${word}`}},
+            //     {r_des: {$regex: `${word}`}} 
+            // ]})
+
+            // words = await Word.find({ $or: [      // Word 모델의 r_word 필드와 r_des 필드에서 쿼리를 포함하는 단어 검색후 최신순으로 정렬하고 3개만 가져오기
+            //         {r_word: {$regex: `${word}`}},
+            //         {r_des: {$regex: `${word}`}} 
+            //     ]})
+            //     .sort({"_id": -1})
+            //     .limit(6)
+           
+            
         }catch(e){
             console.log(e)
         }
